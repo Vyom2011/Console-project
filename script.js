b5e5fb926ee83a4d3c239413638a63ef8e6c3531
Vyom2011 <79401037+Vyom2011@users.noreply.github.com>
2021-05-09 16:18:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var a=10
+var b=5
+
+console.log("I am console screen");
+var first_name="Vyom";
+console.log(first_name);
+
+var last_name="Punjabi";
+console.log(last_name);
+
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var add=a+b;
+console.log(add);
+
+var divide=a/b;
+console.log(divide);
+
+var multiply=a*b;
+console.log(multiply);
+
+var subtraction=a-b;
+console.log(subtraction);
